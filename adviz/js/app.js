f74af4b8312d1644fa25b6baedb351296ecba5ce
JennSol <s0553865@htw-berlin.de>
2020-11-06 13:24:46 +0100

    added marker in map

--- adviz/js/app.js
+++ adviz/js/app.js
@@ -1,4 +1,5 @@
 var mymap = L.map('map').setView([52.456009, 13.527571], 14);
+var marker = L.marker([52.456009, 13.527571]).addTo(mymap);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -6,4 +7,5 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1Ijoia2Fyc3RlbmFic2NoaWVkIiwiYSI6ImNrZ210OGRzaDF1eTAydHRldzdzbTZ0MG8ifQ.Y8abAkOxDNR_Am3Ij1GNzw'
+ 
 }).addTo(mymap);
