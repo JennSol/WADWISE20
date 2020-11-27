4aca8b134a87875b17c0644dd73f4db1361d2553
PyroFourTwenty <kermitthecreeper@gmail.com>
2020-11-27 12:34:50 +0100

    implemented login functionality prototype

--- adviz/js/app.js
+++ adviz/js/app.js
@@ -7,5 +7,50 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1Ijoia2Fyc3RlbmFic2NoaWVkIiwiYSI6ImNrZ210OGRzaDF1eTAydHRldzdzbTZ0MG8ifQ.Y8abAkOxDNR_Am3Ij1GNzw'
- 
+
 }).addTo(mymap);
+
+function generateUsers() {
+    let admina = new User('admin', 'abc', true);
+    let normalo = new User('normalo', 'abc', false);
+    return [admina, normalo];
+}
+
+let users = generateUsers();
+function User(username, password, admin) {
+    this.username = username;
+    this.password = password;
+    this.admin = admin;
+};
+
+function authenticate(username, password) {
+    //hardcoded login data, normally we would call our backend here
+    users.forEach(element => {
+        if (username == element.username && password == element.password) {
+            loginSuccessful();
+        }
+    });
+}
+
+function loginSuccessful(){
+    disableLoginView();
+    enableAdminView();    
+}
+
+function disableLoginView(){
+    loginview = document.getElementById('login_view');
+    loginview.style.display = 'none';
+    
+}
+
+function enableAdminView(){
+    adminview = document.getElementById('admin_view');
+    adminview.style.display = 'block';
+    mymap.invalidateSize();
+}
+
+function login() {
+    let password = document.getElementById('password').value;
+    let username = document.getElementById('user_name').value;
+    authenticate(username, password);
+}
