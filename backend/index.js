778be6a9b0a24d97af045a0b2297581541b52418
PyroFourTwenty <kermitthecreeper@gmail.com>
2020-12-23 20:53:29 +0100

    WIP backend

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const http = require('http')
+const fs = require('fs').promises;
+
+const app = express()
+const port = 3000
+
+app.use(express.static('../adviz'));
+
+app.get('/adviz/login', (req,res) =>{
+    fs.readFile('../adviz/index.html').then(contents=>{
+        res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+    });
+});
+
+app.get('/adviz/contacts', (req, res) => {
+    //api call would be locahost:3000/adviz/contacts?userId=<user id here>
+    console.log(req.query.userId)
+    res.send('Hello '+ req.query.userId )
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
