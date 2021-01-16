653ffd73d1d5925a8ee71b1a89216945d749953c
JennSol <s0553865@htw-berlin.de>
2021-01-16 17:01:42 +0100

    implemented "Create resource"

--- routes/adviz.js
+++ routes/adviz.js
@@ -2,9 +2,11 @@ const express = require('express')
 const path = require('path');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 router.use(bodyParser.json());
 
 const User = require('../models/user');
+const Contact = require('../models/contact');
 const { Mongoose, isValidObjectId } = require('mongoose');
 
 
@@ -30,6 +32,32 @@ router.post('/login', (req,res) =>{
         res.status(500).json({
             error: err
         });
+    });
+});
+
+//create new Contact
+router.post('/contacts',(req,res) =>{
+    const contact = new Contact({
+        _id: mongoose.Types.ObjectId(),
+        title: req.body.title,
+        gender: req.body.gender,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        street: req.body.street,
+        house: req.body.house,
+        postcode: req.body.postcode,
+        city: req.body.city,
+        country: req.body.country,
+        email: req.body.email,
+        other: req.body.other,
+        private: req.body.private,
+        geoCord: req.body.geoCord,
+        owner: req.body.owner
+    });
+    contact.save();
+    res.location('/contacts/' + contact._id);
+    res.status(201).json({
+        contactID: contact._id
     });
 });
 
